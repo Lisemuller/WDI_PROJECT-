fe6e7ed69b45183c2de9a4ff22137756fbd5f3e1
Lise Muller <lisemuller@hotmail.fr>
2016-01-29 16:31:30 +0000

    commiiiiit

--- js/app.js
+++ js/app.js
@@ -1,22 +1,72 @@
 $(function(){
 
-   var solutions = $('.sol'); // transform the solutions in an array
-   // var choices   = ["green", "blue", "pink", "violet"];
+   var solutions = $('.sol'); 
    var choices   = ["pink", "green", "yellow", "blue"];
    var botChoice; 
+   var round = 0;
+   var $containers = $('.container');
+   // var round1 = $('.container').eq(0);
+
 
 // get the random solution by the computer 
-$('.sol').addClass(function() {
-     var index = Math.floor(Math.random() * choices.length);
-     botChoice = choices[index];
-     return botChoice;
-});
+// $('.sol').addClass(function() {
+//      var index = Math.floor(Math.random() * choices.length);
+//      botChoice = choices[index];
+//      return botChoice;
+// });
+
+// make the player choose a combination of 4 colours using the keyboard : 
+
+
+
+// make eventlistener for each key 
+//
+  function getPlayerChoice () {
+    var playerChoice;
+    $(document).on("keyup", function(event) {
+
+      if(event.keyCode === 81) {
+        playerChoice = "pink";
+      }
+      else if(event.keyCode === 87) {
+        playerChoice = "blue";
+      }
+      else if(event.keyCode === 69) {
+        playerChoice = "green";
+      }
+      else if (event.keyCode === 82) {
+        playerChoice = "yellow";
+      }
+      console.log(playerChoice);
+    })
+    return playerChoice;
+  };
+
+getPlayerChoice();
 
 
 
 
+// make the computer give the feedback 
+
+// make it play 
+//  if ((the feedback of the computer) < 4 green) {
+//   round ++;
+//   else if ((the feedback of the computer) < 4 green) && (round <=8))  {
+//  console.log ("Well done ! You found the solution in" + round + "!");
+//   }
+//  else {
+//     console.log("You loose against the evil rabbit generator")
+// }
+//  }
 
 
+// associate each containers to each round
+// function getRound(){
+//   console.log($containers.eq(round));
+// }
+
+// getRound();
 
 
 
