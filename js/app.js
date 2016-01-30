9668903a364ddab5aacd0142f1925f7fb4be9278
Lise Muller <lisemuller@hotmail.fr>
2016-01-30 15:08:17 +0000

    commiiiit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(function(){
 
-   var solutions = $('.sol'); 
+   var $solutions = $('.sol'); 
    var choices   = ["pink", "green", "yellow", "blue"];
    var botChoice; 
    var round = 0;
@@ -18,15 +18,13 @@ $('.sol').addClass(function() {
 
 // make the player choose a combination of 4 colours using the keyboard : 
 
-// $('#').addClass(function(){
-//   return getPlayerChoice();
-// });
 
 
 // make eventlistener for each key 
 //
 var playerChoice = null;
 var choiceCount = 0;
+
 $(document).on("keyup", function(event) {
 
   if(event.keyCode === 81) {
@@ -46,19 +44,47 @@ $(document).on("keyup", function(event) {
   }
 
 
+  // make the choice enter in the first container
+
   if(playerChoice) {
     var $currentRound = $('.container').eq(round);
     var $currentCells = $currentRound.find('.large');
     $currentCells.eq(choiceCount).addClass(playerChoice);
-
     choiceCount++;
   }
 
+  if (jQuery.inArray( "pink", $currentCells ) )  {
+    console.log("hello");
+  }
+
+  
 });
 
 
 
+
+
 // make the computer give the feedback 
+
+// function evaluate() {
+//   switch (botChoice) {
+//     case "rock":
+//     if (botChoice == "paper") { botScore++; } 
+//     if (botChoice == "scissors") { userScore++; }
+//     break;
+
+//     case "paper":
+//     if (botChoice == "rock") { userScore++; } 
+//     if (botChoice == "scissors") { botScore++; }
+//     break;
+
+//     case "scissors":
+//     if (botChoice == "paper") { userScore++; } 
+//     if (botChoice == "rock") { botScore++; }
+//     break;
+//   }
+// }
+
 
 // make it play 
 //  if ((the feedback of the computer) < 4 green) {
